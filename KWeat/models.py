fd8b14635ecc5474eb28ea267613217ad1e84026
jinoo98 <bitnari123@naver.com>
2022-11-02 19:32:06 +0900

    db create

--- KWeat/models.py
+++ KWeat/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    id = models.AutoField(primary_key=True)
+    Res_name = models.CharField(max_length=15, default=' ')
+    category = models.CharField(max_length=10, default=' ')
+    distance = models.IntegerField()
+
+class Menu(models.Model):
+    Res_id = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    Menu_name = models.CharField(max_length=20, default=' ')
+    price = models.IntegerField()
+
+# class Restaurant(models.Model):
+#     id = models.CharField(max_length=200, default=' ')
+#     name = models.TextField()
+#     create_date = models.DateTimeField()
+#     def __str__(self):
+#         return self.subject
+#
+#
+#
+# class Menu(models.Model):
+#     subject = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     content = models.TextField()
+#     create_date = models.DateTimeField()
