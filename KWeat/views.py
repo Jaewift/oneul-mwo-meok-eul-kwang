34d77cf2e9a5bd2083bcc7eb1dde050c3c4fb685
jinoo98 <bitnari123@naver.com>
2022-11-07 19:54:56 +0900

    html css

--- KWeat/views.py
+++ KWeat/views.py
@@ -3,10 +3,24 @@ from .models import Restaurant, Menu
 from .forms import RestaurantForm, MenuForm
 
 
-def index(request):
+def filter(request):
     '''
     kweat 필터
     '''
+    res_object = Restaurant.objects.get(pk=1)
+    menu_object = Menu.objects.all()
+    return render(request, 'kweat_filter_search.html', {'res_object': res_object, 'menu_object': menu_object})
+
+def roulette(request):
+    '''
+    kweat 룰렛
+    '''
+    return render(request, 'kweat_roulette.html')
+
+def questions(request):
+    '''
+    kweat 스무고개
+    '''
     # question_list = Restaurant.objects.order_by('')
     # context = {'Res_name': question_list}
-    return render(request, 'kweat_filter_search.html')
+    return render(request, 'kweat_twenty_questions.html')
