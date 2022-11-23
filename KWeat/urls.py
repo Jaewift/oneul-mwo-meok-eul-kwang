d44997573b63865065f7ec44bbf068c1bded587f
DaehanChoi <104803789+dhchoi98@users.noreply.github.com>
2022-11-23 20:30:47 +0900

    Update urls.py
    URL fix

--- KWeat/urls.py
+++ KWeat/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'kweat'
 
 urlpatterns = [
-    path('/', views.filter, name='home'),
+    path('', views.filter, name='home'),
     path('filter/', views.filter, name='filter'),
     path('roulette/', views.roulette, name='roulette'),
     path('questions/', views.questions, name='questions'),
